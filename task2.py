843e78bf9ed789ffcd17b34f5f9399216e4b791b
Bekzat-Chauvbayev <kazna51k@gmail.com>
2024-11-08 09:24:14 +0500

    task1-2

---
+++ task2.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def findLengthOfLCIS(self,nums):
+        max_count = 1
+        count = 1
+
+        for i in range(o,len(nums)):
+            if i == len(nums)-1:
+                break
+            if nums[i]<nums[i+1]:
+                count+=1
+                max_count = max(max_count,count)
+            else:
+                count =1
+        return max_count
